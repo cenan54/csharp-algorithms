7f2a25af15319068acb7ce3595ac7c7a74ae43d9
Mustafa Cenan <54mustafacenan54@gmail.com>
2024-07-31 00:14:58 +0300

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using TwoSum.Solution;
+
+int[] array = { 2, 6, 10 };
+int target = 8;
+
+Solution solution = new Solution();
+
+int[] solutionArray = solution.TwoSum(array, target);
+
+for (int i = 0; i < solutionArray.Length; i++)
+{
+    Console.Write(solutionArray[i]);
+    if (i!=solutionArray.Length-1)
+    {
+        Console.WriteLine(", ");
+    }
+}
---
+++ TwoSum/Solution/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum.Solution
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            for (int x = 0; x < nums.Length; x++)
+            {
+                int remain = target - nums[x];
+
+                for (int y = x + 1; y < nums.Length; y++)
+                {
+                    if (nums[y] == remain)
+                    {
+                        return new int[] { x, y };
+                    }
+                }
+            }
+
+            throw new Exception("listedekiler hedefi tamamlamıyor");
+        }
+    }
+}
